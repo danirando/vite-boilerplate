007709734348f5dd19a849f175bb68ec3c0b2fb8
danirando <danielerando@tiscali.it>
2025-05-30 18:30:52 +0200

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import DefaultLayout from "./layouts/DefaultLayout";
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<HomePage />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Main.jsx
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export default function Main() {
-  return <main></main>;
-}
